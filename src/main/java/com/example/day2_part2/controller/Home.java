01464e0a780fa3689e737371c556c69496b20310
Ankur Mittal <ankurmittal9081@gmail.com>
2026-01-19 15:20:05 +0530

    Added our first api

--- src/main/java/com/example/day2_part2/controller/Home.java
+++ src/main/java/com/example/day2_part2/controller/Home.java
@@ -3,13 +3,26 @@ package com.example.day2_part2.controller;
 import com.example.day2_part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
 @RestController
 public class Home {
    @GetMapping("/")
-    public StudentModel getStudent(){
+    public ArrayList<StudentModel> getStudent(){
        StudentModel student=new StudentModel(1,"ankur mittal","ankurmittal9081@gmail.com");
-       return student;
+       StudentModel student1=new StudentModel(1,"ankur mittal","ankurmittal9081@gmail.com");
+       StudentModel student2=new StudentModel(1,"ankur mittal","ankurmittal9081@gmail.com");
+       StudentModel student3=new StudentModel(1,"ankur mittal","ankurmittal9081@gmail.com");
+       StudentModel student4=new StudentModel(1,"ankur mittal","ankurmittal9081@gmail.com");
+
+
+       ArrayList<StudentModel> l=new ArrayList<>();
+       l.add(student);
+       l.add(student1);
+       l.add(student2);
+       l.add(student3);
+       l.add(student4);
+       return l;
 
    }
 
